1e161a60c64aeae5fa6a71651071c20a00df3953
lyn <Lyn>
2018-07-27 12:55:23 +0800

    update

---
+++ src/com/lyn/Main.java
@@ -0,0 +1,11 @@
+package com.lyn;
+
+import jianzhioffer.CloneRandomListNode;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        CloneRandomListNode cloneRandomListNode = new CloneRandomListNode();
+    }
+}
